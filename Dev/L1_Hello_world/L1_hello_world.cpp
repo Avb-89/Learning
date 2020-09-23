53f6af78494a05c4e8374548673bab4de3708ea6
Alexey Vadimovich <avb-89@outlook.com>
2020-09-23 15:27:46 +0400

    Lesson 1 (Hello World)

---
+++ Dev/L1_Hello_world/L1_hello_world.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************
+* Autor: Alexey Vadimovich
+* Annotation: This program prints a message to the screen in the console
+* Date: 23-09-2020
+********************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "HHHH       HHHH EEEEEEEEE  LLLLL            LLLLL           OOOOOOOOOOOOOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEEEEEEEE  LLLLL            LLLLL           OOOOOOOOOOOOOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEEEEEEEE  LLLLL            LLLLL           OOOOOOOOOOOOOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEE        LLLLL            LLLLL           OOO        OOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEE        LLLLL            LLLLL           OOO        OOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEEEEE     LLLLL            LLLLL           OOO        OOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "OOOHHHHHHHHOOOH EEEEEE     LLLLL            LLLLL           OOO        OOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEEEEE     LLLLL            LLLLL           OOO        OOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEE        LLLLL            LLLLL           OOO        OOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEE        LLLLL            LLLLL           OOO        OOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEEEEEEEE  LLLLLLLLLLLLLLL  LLLLLLLLLLLLLLL OOOOOOOOOOOOOO				          " << std::endl;
+	std::cout << "HHHH       HHHH EEEEEEEEE  LLLLLLLLLLLLLLL  LLLLLLLLLLLLLLL OOOOOOOOOOOOOO !!!! !!!! !!!!   " << std::endl;
+	std::cout << "HHHH       HHHH EEEEEEEEE  LLLLLLLLLLLLLLL  LLLLLLLLLLLLLLL OOOOOOOOOOOOOO !!!! !!!! !!!!   " << std::endl;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
