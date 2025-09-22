47da6d16046b9a49193d27e680c7b76b9243e5f8
Alexey Vadimovich <avb-89@outlook.com>
2025-09-22 20:43:40 +0300

    start

---
+++ Dev/First_program/First_program/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  First_program
+//
+//  Created by Alexey Vadimovich on 9/22/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
