4ad93fa3919b71899355228c1bcd5d0d8decdc8e
Alexey Vadimovich <avb-89@outlook.com>
2025-09-23 00:15:43 +0300

    DT_start

---
+++ Dev/Data_type/Data_type/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data_type
+//
+//  Created by Alexey Vadimovich on 9/23/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
